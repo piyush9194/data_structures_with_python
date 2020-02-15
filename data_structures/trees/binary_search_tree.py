6a463ec2a8f9983082181fff8b74348e6be99783
piyush.gupta <piyush.gupta@grabtaxi.com>
2020-02-15 17:16:07 +0800

    Modified the binary search tree

--- data_structures/trees/binary_search_tree.py
+++ data_structures/trees/binary_search_tree.py
@@ -1,8 +1,10 @@
 """"
    Implement a binary search trees.
-        Insertion : O(logn)     Worst Case: O(n)
-        Deletion: O(logn)       Worst Case: O(n)
-        Search:   O(logn)       Worst Case: O(n)
+        Insertion : O(h)     Worst Case: O(n)
+        Deletion: O(h)       Worst Case: O(n)
+        Search:   O(h)       Worst Case: O(n)
+
+        where h is height of tree.
 
 """
 
@@ -13,51 +15,95 @@ class TreeNode():
         self.left = None
         self.right = None
 
+
 class BinarySearchTree():
     def __init__(self,root=None):
         self.root = root
 
-
     def insert(self,data,root):
-        # print(f"data inseretd is {data}")
         node = TreeNode(data)
         current = root
         if self.root is None:
             self.root = node
         elif data <= current.data:
-            if current.left is None:
+            if current.left:
+                self.insert(data, current.left)
+            else:
                 current.left = node
-            else:
-                self.insert(data,current.left)
         elif data> current.data:
-            if current.right is None:
-                current.right = node
+            if current.right:
+                self.insert(data, current.right)
             else:
-                self.insert(data,current.right)
+                current.right = node
 
+    def search(self,data,root):
+        current = root
+        if current:
+            if current.data == data:
+                return True
+            elif data < current.data:
+                return(self.search(data, current.left))
+            elif data > current.data:
+                return(self.search(data, current.right))
+        else:
+            return False
+
+    def delete(self,data):
+        self._delete(self.root,data)
+
+
+    def _delete(self,current,data,parent=None,isLeft=None):
+        if current:
+            if current.data == data:
+                print(f"deleting the key:{data}")
+                if isLeft:
+                    parent.left = None
+                else:
+                    parent.right = None
+            elif data>current.data:
+                self._delete(current.right,data,current,False)
+            elif data < current.data and current.left:
+                self._delete(current.left,data,current,True)
+
+        else:
+            print("Key not found")
 
     def inorder_traversal(self,root):
-        traversal = " "
         current = root
-
         if current is None:
             return
         else:
             self.inorder_traversal(current.left)
-            print(current.data)
+            print(current.data,end= "--> ")
             self.inorder_traversal(current.right)
 
 
     def desc_traversal(self,root):
-        traversal = " "
         current = root
-
         if current is None:
             return
         else:
             self.desc_traversal(current.right)
-            print(current.data)
+            print(current.data,end= "--> ")
             self.desc_traversal(current.left)
+
+    def _isBST(self,root,min,max):
+        current = root
+        if current is None:
+            return True
+        elif current.data < min or current.data>max:
+            return False
+        else:
+            return (self._isBST(current.left,min,current.data)
+            and self._isBST(current.right,current.data,max))
+
+    def isBST(self,root):
+        if self.root is None:
+            return True
+        else:
+            min = -1000000
+            max = 10000000
+            return(self._isBST(root,min,max))
 
 
 
@@ -71,13 +117,12 @@ tree.insert(10,tree.root)
 tree.insert(4,tree.root)
 tree.insert(40,tree.root)
 
-# trees.inorder_traversal(trees.root)
-
+tree.inorder_traversal(tree.root)
+print("/n")
 tree.desc_traversal(tree.root)
-
-
-
-
+print("/n")
+print(tree.search(15,tree.root))
+print(tree.isBST(tree.root))
 
 
 
